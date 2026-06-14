a1895b6082419de905125cc6199152e11e1851cf
Kareemah Ahmad Yusuf <akareema201@gmail.com>
2026-06-14 09:29:59 -0700

    add updated_at to UserProfile model

--- users/models.py
+++ users/models.py
@@ -28,6 +28,7 @@ class UserProfile(models.Model):
     language_preference = models.CharField(max_length=20, default='english')
     channel = models.CharField(max_length=20, default='app')
     waste_type = models.CharField(max_length=20, blank=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.user} profile"
